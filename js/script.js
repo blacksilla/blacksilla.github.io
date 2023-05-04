cbb2a743bb8f02cf417ce75f41e8918917fdad60
blacksilla <afonsosilva1@ua.pt>
2023-05-04 17:37:58 +0100

    linktree added and some css/js

--- js/script.js
+++ js/script.js
@@ -1,66 +1,57 @@
 //toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 let currentSectionIndex = 0;
 
-menuIcon.onclick = () =>{
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 //scroll sections
 let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a")
+let navLinks = document.querySelectorAll("header nav a");
 
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+      // active sections for animation on scroll
 
+      sec.classList.add("show-animate");
+    }
+    // for animations repeating on scroll
+    else {
+      sec.classList.remove("show-animate");
+    }
+  });
+  let header = document.querySelector("header");
 
+  header.classList.toggle("sticky", window.scrollY > 100);
 
+  // remove toggle icon and navbar when click navbar links (scrol)
 
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 
-window.onscroll = () =>{
-    sections.forEach(sec =>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop-100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  //animation footer on scroll
 
-        if(top>=offset && top < offset + height){
-            navLinks.forEach(links =>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-            // active sections for animation on scroll
-            
-            sec.classList.add('show-animate');
-            sec.classList.add("locked");
-            
-        }
-        // for animations repeating on scroll
-        else{
-            sec.classList.remove('show-animate');
-            sec.classList.remove("locked");
-           
-        }
-    });
-    let header = document.querySelector("header");
+  let footer = document.querySelector("footer");
+  let bodyHeight = document.body.clientHeight;
+  let scrollHeight = document.scrollingElement.scrollHeight;
 
-    header.classList.toggle('sticky',window.scrollY>100);
-
-    // remove toggle icon and navbar when click navbar links (scrol)
-
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-
-
-    //animation footer on scroll
-
-    let footer = document.querySelector('footer');
-    let bodyHeight = document.body.clientHeight;
-    let scrollHeight = document.scrollingElement.scrollHeight;
-
-    footer.classList.toggle('show-animate', bodyHeight + window.scrollY >= scrollHeight);
-}
-
-
-  
+  footer.classList.toggle(
+    "show-animate",
+    bodyHeight + window.scrollY >= scrollHeight
+  );
+};
